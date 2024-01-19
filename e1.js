95f254a9c6809878a16a905a427518d73ede3158
ItaloFernande <italofernandez777@gmail.com>
2024-01-19 11:21:06 -0500

    ejer 2 15%

--- e1.js
+++ e1.js
@@ -4,16 +4,36 @@ function devolverboton(i,j)
 }
 
 let botono = null
+let botonoT = null
+let botonoB = null
+let botonoL = null
+let botonoR = null
 
 console.log("XD")
 
 function botoncarsi(i, j) 
 {
     console.log("Hizo Click en", i, j);
-    
+
     botono = devolverboton(i, j);
+    botonoT = devolverboton((1+i), j);
+    botonoB = devolverboton((i-1), j);
+    botonoL = devolverboton(i, (j-1));
+    botonoR = devolverboton(i, (1+j));
     
-    let idbotono = i + "_" + j;
+    let idbotono1 = i + "_" + j;
+    let idbotonoT = i + "_" + (1+j);
+    let idbotonoB = i + "_" + (j-1);
+    let idbotonoL = (i-1) + "_" + j;
+    let idbotonoR = (i+1) + "_" + j;
+
+
+    botono.innerHTML = "X";
+    botonoT.innerHTML = "X";
+    botonoB.innerHTML = "X";
+    botonoL.innerHTML = "X";
+    botonoR.innerHTML = "X";
+
 }
 
  main()
