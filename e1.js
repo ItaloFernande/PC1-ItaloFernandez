baa55623781c7bf9d7b6d9b524e96c90be984cd1
ItaloFernande <italofernandez777@gmail.com>
2024-01-19 11:34:32 -0500

    espejo 20%

--- e1.js
+++ e1.js
@@ -9,30 +9,43 @@ let botonoB = null
 let botonoL = null
 let botonoR = null
 
-console.log("XD")
 
 function botoncarsi(i, j) 
 {
-    console.log("Hizo Click en", i, j);
-
     botono = devolverboton(i, j);
     botonoT = devolverboton((1+i), j);
     botonoB = devolverboton((i-1), j);
     botonoL = devolverboton(i, (j-1));
     botonoR = devolverboton(i, (1+j));
-    
-    let idbotono1 = i + "_" + j;
-    let idbotonoT = i + "_" + (1+j);
-    let idbotonoB = i + "_" + (j-1);
-    let idbotonoL = (i-1) + "_" + j;
-    let idbotonoR = (i+1) + "_" + j;
-
 
     botono.innerHTML = "X";
-    botonoT.innerHTML = "X";
-    botonoB.innerHTML = "X";
-    botonoL.innerHTML = "X";
-    botonoR.innerHTML = "X";
+    if(botonoT)
+    {
+        botonoT.innerHTML = "X";
+    }
+
+    if(botonoB)
+    {
+        botonoB.innerHTML = "X";
+    }
+
+    if(botonoL)
+    {
+        botonoL.innerHTML = "X";
+    }
+
+    if(botonoR)
+    {
+        botonoR.innerHTML = "X";
+    }
+
+    
+    
+
+    
+    
+    
+    
 
 }
 
