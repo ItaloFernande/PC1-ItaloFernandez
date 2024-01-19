430168a9a7638541f835c458e6219b71f4d743cf
ItaloFernande <italofernandez777@gmail.com>
2024-01-19 11:52:15 -0500

    todo si

--- e1.js
+++ e1.js
@@ -9,6 +9,12 @@ let botonoB = null
 let botonoL = null
 let botonoR = null
 
+let botonoiz = null
+let botonoTiz = null
+let botonoBiz = null
+let botonoLiz = null
+let botonoRiz = null
+
 
 function botoncarsi(i, j) 
 {
@@ -18,10 +24,21 @@ function botoncarsi(i, j)
     botonoL = devolverboton(i, (j-1));
     botonoR = devolverboton(i, (1+j));
 
+
+    botonoiz = devolverboton(15 - i, 15 -j);
+    botonoTiz = devolverboton(15 -(1+i), 15 -j);
+    botonoBiz = devolverboton(15 -(i-1), 15 -j);
+    botonoLiz = devolverboton(15 -i, 15 -(j-1));
+    botonoRiz = devolverboton(15 -i, 15 -(1+j));
+
+
+        //Parte DERECHA
+
     botono.innerHTML = "X";
     if(botonoT)
     {
         botonoT.innerHTML = "X";
+        
     }
 
     if(botonoB)
@@ -39,14 +56,36 @@ function botoncarsi(i, j)
         botonoR.innerHTML = "X";
     }
 
-    
-    
+    //Parte IZQUIERDA
 
-    
-    
-    
-    
+    botonoiz.innerHTML = "X"
 
+    if(botonoTiz)
+    {
+        botonoTiz.innerHTML = "X";
+    }
+
+    if(botonoBiz)
+    {
+        botonoBiz.innerHTML = "X";
+    }
+
+    if(botonoLiz)
+    {
+        botonoL.innerHTML = "X";
+    }
+
+    if(botonoRiz)
+    {
+        botonoRiz.innerHTML = "X";
+    }
+
+
+    /*botono.disabled = true;
+    botonoL.disabled = true;
+    botonoR.disabled = true;
+    botonoT.disabled = true;
+    botonoB.disabled = true;*/
 }
 
  main()
